5d01a1b3270643d7f8ba200206739df9e196dee5
Steve Graf <stevencgraf@gmail.com>
2025-11-24 23:58:13 -0500

    Vite config update to handle more pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        // Defines 'index.html' as the main entry point
+        main: resolve(__dirname, 'index.html'),
+        // Adds 'odds.html' as a second entry point
+        odds: resolve(__dirname, 'odds.html'),
+      },
+    },
+  },
+});
